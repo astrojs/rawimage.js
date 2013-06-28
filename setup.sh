9748755f4d06c58d6f3905ac1d9eb32e12aef262
Amit Kapadia <amit@zooniverse.org>
2013-06-28 14:52:59 -0500

    setup script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir -vp examples/lib
 mkdir -vp examples/data
 
 # Download JavaScript dependencies
-curl "https://raw.github.com/astrojs/fitsjs/rewrite/lib/fits.js" -o 'examples/lib/fits.js'
+curl -k "https://raw.github.com/astrojs/fitsjs/rewrite/lib/fits.js" -o 'examples/lib/fits.js'
 
 # Download data for examples
 curl "http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/data/pub/CFHTSG/W3-2%2B0.G.fits%5B16094%3A16605%2C7328%3A7839%5D" -o "examples/data/m101_g.fits"
