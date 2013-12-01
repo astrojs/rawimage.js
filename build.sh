f9a3ead06c2a5231140412512600874b496bf0dd
Amit Kapadia <amit@zooniverse.org>
2013-12-01 12:54:33 -0600

    remove more old files

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ for f in $FILES
 do
   sed 's/^/  /' src/$f > tmp/$f
 done
-cat src/start.js ${FILES//  /tmp\/} src/end.js > ruse.js
-node_modules/.bin/uglifyjs ruse.js -o ruse.min.js
+cat src/start.js ${FILES//  /tmp\/} src/end.js > rawimage.js
+node_modules/.bin/uglifyjs rawimage.js -o rawimage.min.js
 rm -rf tmp
--- src/WebFITS.coffee
+++
@@ -1,6 +0,0 @@
-@astro = {} unless @astro?
-
-WebFITS = {}
-WebFITS.version = '0.4.3'
-
-@astro.WebFITS = WebFITS
